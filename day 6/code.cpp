bddb04070766283e0bac3fd75a254a8d5c93ef94
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-06-30 21:51:11 +0530

    pattern question sigma batch class question

--- day 6/code.cpp
+++ day 6/code.cpp
@@ -75,6 +75,18 @@ for (int i = 1; i <= n; i++){
 }
 */
 
+// character pyramid pattern
+
+/*
+
+A
+B C
+D E F
+G H I J
+
+*/
+
+/*
 char ch = 'A';
 for (int i = 1; i <= n; i++){
     for (int j = 1; j <= i; j++){
@@ -83,7 +95,209 @@ for (int i = 1; i <= n; i++){
     }
     cout << endl;
 }
+*/
 
+// Hollow rectangle pattern
+
+/*
+
+*****
+*   *
+*   *
+*****
+
+*/
+/*
+for (int i = 1; i <= n; i++){
+
+    cout << "*"; // for first star
+
+    for (int j = 1; j <= n-1; j++){
+        if (i == 1 || i == n){
+            cout << "*";
+        } else {
+            cout << " ";
+        }
+    }
+
+    cout << "*" << endl;
+}
+
+*/
+
+// inverted and rotated half pyramid pattern
+
+/*
+
+   *
+  **
+ ***
+****
+
+*/
+
+/*
+
+for (int i = 1; i <= n; i++){ // outer loop
+
+    // spaces
+    for (int j = 1; j <= n-i; j++){
+        cout << " ";
+    }
+
+    // stars
+    for (int j = 1; j <= i; j++){
+        cout << "*";
+    }
+
+    cout << endl;
+
+}
+
+*/
+
+// floyd's triangle pattern
+
+/*
+
+1 
+2 3
+4 5 6
+7 8 9 10
+
+*/
+
+/*
+
+int num = 1;
+
+for (int i = 1; i <= n; i++){
+    for (int j = 1; j <= i; j++){
+        cout << num << " ";
+        num++;
+    }
+
+    cout << endl;
+}
+
+*/
+
+// diamond pattern
+
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+
+
+/*
+
+// first pyaramid
+
+for (int i = 1; i <= n; i++){
+
+    // spaces
+    for (int j = 1; j <= n-i; j++){
+        cout << " ";
+    }
+
+    // stars
+    for (int j = 1; j <= 2*i-1; j++){
+        cout << "*";
+    }
+    
+    cout << endl;
+
+}
+
+// second pyramid
+
+for (int i = n; i >= 1; i--){
+    
+    // spaces
+    for (int j = 1; j <= n-i; j++){
+        cout << " ";
+    }
+
+    // stars
+    for (int j = 1; j <= 2*i-1; j++){
+        cout << "*";
+    }
+
+    cout << endl;
+
+}
+
+*/
+
+// butterfly pattern
+
+/*
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
+
+/*
+
+for (int i = 1; i <= n; i++){
+    
+    for (int j = 1; j <= i; j++){
+        cout << "*";
+    }
+
+    for (int j = 1; j <= 2*(n-i); j++){
+        cout << " ";
+    }
+
+    for (int j = 1; j <= i; j++){
+        cout << "*";
+    }
+
+    cout << endl;
+}
+
+for (int i = n; i >= 1; i--){
+
+    for (int j = 1; j <= i; j++){
+        cout << "*";
+    }
+
+    for (int j = 1; j <= 2*(n-i); j++){
+        cout << " ";
+    }
+
+    for (int j = 1; j <= i; j++){
+        cout << "*";
+    }
+
+    cout << endl;
+}
+
+*/
 
     return 0;
 }
+
+
+
+
