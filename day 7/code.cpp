6a286c360eee3eb1bafc8f8bb7117a1241718976
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-01 23:48:27 +0530

    function c++ basic concept

--- day 7/code.cpp
+++ day 7/code.cpp
@@ -16,6 +16,12 @@ void sayNamaste(); // forward declaration -- agar kisi bhi function ko main() ke
 
 int sum (int a, int b);
 
+int prod (int a, int b);
+
+bool isEven (int num);
+
+int fact(int n);
+
 
 int main(){
     assistant(); // Hello :) // work done!
@@ -23,6 +29,14 @@ int main(){
     sayNamaste();
 
     cout << sum (2, 4) << endl;
+
+    cout << prod (2, 4) << endl;
+
+    cout << isEven(20) << endl; // 1
+    cout << isEven(19) << endl; // 0
+
+    cout << "factorial of " << 5 << " is " << fact(5) << endl; // 120
+ 
     return 0;
 }
 
@@ -32,4 +46,24 @@ void sayNamaste(){
 
 int sum (int a, int b){
     return a + b;
+}
+
+int prod(int a, int b){
+    return a * b;
+}
+
+bool isEven (int num){
+    if (num % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int fact(int n){
+    int fact = 1;
+    for (int i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
 }
