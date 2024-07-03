5d954d2541d9b875faec2f8b85f55c8847a98c26
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-03 15:56:42 +0530

    convert decimal number into binary number

---
+++ day 8/decimal_to_binary.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int decToBin(int decNo){
+    int n = decNo;
+    int revBinNo = 0;
+
+    while (n >= 1){
+        int lastDigit = n % 2;
+        revBinNo = revBinNo * 10 + lastDigit;
+        n = n / 2;
+    }
+
+    int binNo = 0;
+
+    while (revBinNo > 0){
+        int lastDigit = revBinNo % 10;
+        binNo = binNo * 10 + lastDigit;
+        revBinNo /= 10;
+    }
+
+    return binNo;
+}
+
+int main(){
+
+    int decNo;
+    cin >> decNo;
+
+    cout << decToBin(decNo) << endl;
+
+    return 0;
+}
