ce09ec1f71378e9981151f1a732c85d3e79058bc
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-05 12:40:33 +0530

    added a question based on pointer and reference variables

--- day 9/assingment.cpp
+++ day 9/assingment.cpp
@@ -24,6 +24,19 @@ int main(){
     multiplyBy2(k, l, m);
     cout << k << l << m << endl;
 
+    // Q3 what will be output of following code.
+
+    int a = 32;
+    int *ptrr = &a;
+
+    char ch = 'A';
+    char &cho = ch;
+
+    cho += a;
+    *ptrr += ch;
+
+    cout << a << " , " << ch << endl; // 129, a
+
 
 
     return 0;
