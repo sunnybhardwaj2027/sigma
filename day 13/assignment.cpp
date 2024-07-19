5e8343987cc3b5e38f797532d90bb4f6933631be
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-19 15:08:58 +0530

    removed transpose matrix solution bcoz there was little issue in the code

--- day 13/assignment.cpp
+++ day 13/assignment.cpp
@@ -14,23 +14,6 @@ void countof7(int arr[][3], int n, int m){
     cout << "count of '7' = " << countof7 << endl;
 }
 
-void printTransposeMatrix(int matrix[][3], int row , int column){
-    int transposeMatrix[][2] = {{0}};
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < column; j++){
-            transposeMatrix[j][i] = matrix[i][j];
-        }
-    }
-
-    // print transpose matrix
-    for (int i = 0; i < column; i++){
-        for (int j = 0; j < row; j++){
-            cout << transposeMatrix[i][j] << "   ";
-        }
-        cout << endl;
-    }
-}
-
 int main(){
     /*
     Question 1 : Print the number of all 7’s that are in the 2d array.
@@ -56,25 +39,6 @@ int main(){
    }
 
    cout << "sum of 2nd row numbers = " << sum << endl;
-
-   /*
-    Question 3 : Write a program to Find Transpose of a Matrix. 
-    What is Transpose?
-    Transpose of a matrix is the process of swapping the rows to columns. For a 2x3 
-    matrix,
-    Matrix
-    a11 a12 a13 
-    a21 a22 a23
-    Transposed Matrix 
-    a11 a21
-    a12 a22
-    a13 a23
-
-    */
-
-   int row = 2, column = 3;
-   int matrix[][3] = { {2, 3, 7}, {5, 6, 7} };
-   printTransposeMatrix(matrix, 2, 3);
 
     return 0;
 }
