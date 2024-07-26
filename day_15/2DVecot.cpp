5df5f2e224ef4164609fb7bbe96770f953d11e91
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-26 13:02:11 +0530

    2d vector

---
+++ day_15/2DVecot.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <vector<int>> matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    for (int i = 0; i < matrix1.size(); i++) {
+        for (int j = 0; j < matrix1[i].size(); j++){
+            cout << matrix1[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    // note-- in case of 2d vectors no of column for every row can be different which was not possible for 2d array.
+
+    vector <vector<int>> matrix2 = {{1, 2, 3}, {4, 5}, {7}};
+
+    for (int i = 0; i < matrix2.size(); i++) {
+        for (int j = 0; j < matrix2[i].size(); j++){
+            cout << matrix2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    // predict the output
+    vector <int> vec;
+    for (int i = 0; i < 5; i++){
+        vec.push_back(i);
+    }
+
+    cout << vec.size() << endl;
+    cout << vec.capacity() << endl;
+    return 0;
+}
