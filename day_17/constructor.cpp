43891b701761ebe44ca3c31bc840ea74ff9f73ea
sunnybhardwaj2027 <sunnybhardwaj7549@gmail.com>
2024-07-27 15:36:06 +0530

    constructor in class

---
+++ day_17/constructor.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class Car {
+    string name;
+    string color;
+
+public:
+
+    // multiple constructor is possible -- consotructor overloading -- constructor is being called according to parameters.
+    // non parameterized constructor
+    Car(){ // not return type for constructor
+        cout << "constructor is called. object is being created..\n";
+        cout << "not parameterized constructor is called.\n";
+    }
+
+    // parameterized constructor
+    Car(string nameVal, string colorVal){
+        cout << "consturctor is called. object is being created..\n";
+        cout << "parameterized constructor is called.\n";
+        name = nameVal;
+        color = colorVal;
+    }
+
+    void start(){
+        cout << "car has started..\n";
+    }
+    
+    void stop(){
+        cout << "car has stopped..\n";
+    }
+
+    //getters
+    string getName(){
+        return name;
+    }
+};
+
+int main(){
+    Car c1; /* constructor is called. object is being created..
+not parameterized constructor is called. */
+
+    Car c2("maruti 800", "white"); /*  output -- consturctor is called. object is being created..
+parameterized constructor is called. */
+    cout << c2.getName() << endl; // output -- maruti 800
+    return 0;
+}
